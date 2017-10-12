c2aa29b6b0b589ab7318d8a4133b8129d74a6eb7
Felix Franz <felix-franz@t-online.de>
2017-10-12 21:11:31 +0200

    #6 added a multi threading for sending commands to vlc

--- src/main/java/backend/general/vlcHolder/impl/VLCHolder.java
+++ src/main/java/backend/general/vlcHolder/impl/VLCHolder.java
@@ -3,7 +3,11 @@ package backend.general.vlcHolder.impl;
 import backend.general.Factory;
 import backend.general.vlc.VLCCommand;
 import backend.general.vlc.VLC;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.apache.tomcat.jni.Time;
 
+import java.util.LinkedList;
+import java.util.concurrent.*;
 import java.util.logging.Level;
 
 /**
@@ -26,10 +30,28 @@ public class VLCHolder extends backend.general.vlcHolder.VLCHolder {
 
     public void runCommand(VLCCommand command){
         Factory.getLogger().log(Level.INFO,"used command " + command + " on all vlc instances!");
+
+        ExecutorService executorService = Executors.newFixedThreadPool(Factory.getSettings().getMaxVLCConnectionThreads());
+        long executeTime = System.currentTimeMillis() + 100;
+
         for (VLC vlc : vlcs){
-            //TODO run everything simulanious! (multithreading) --> use vlc as worker and submit command using generic submit function
-            if (!vlc.runCommand(command))
-                Factory.getLogger().log(Level.WARNING, "Failed to run " + command.getCommand() + " on " + vlc.getName());
+
+            Runnable runCommand = new Runnable() {
+                @Override
+                public void run() {
+                    while(System.currentTimeMillis() < executeTime) {
+                        try {
+                            Thread.currentThread().sleep((executeTime - System.currentTimeMillis()) / 2);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();        //ToDo handle exception
+                        }
+                    }
+                    if (!vlc.runCommand(command))
+                        Factory.getLogger().log(Level.WARNING, "Failed to run " + command.getCommand() + " on " + vlc.getName());
+                }
+            };
+            executorService.submit(runCommand);
         }
+        executorService.shutdown();
     }
 }
