f0c1c5b969557c741e61a19e713394734ce2a948
Felix Franz <felix-franz@t-online.de>
2017-10-21 10:34:11 +0200

    #21 added StringCutter

---
+++ src/main/java/backend/utils/StringCutter.java
@@ -0,0 +1,25 @@
+package backend.utils;
+
+/**
+ * Created by Felix on 21.10.2017.
+ */
+public class StringCutter {
+    String input;
+
+    public StringCutter(String input){
+        this.input = input;
+    }
+
+    /**
+     * cuts the string
+     *
+     * @param begin after this string will be extracted
+     * @param end before this string will be extracted
+     * @return cutted string
+     */
+    public String cut(String begin, String end){
+        int startIndex = input.indexOf(begin) + begin.length();
+        int endIndex = input.indexOf(end, startIndex);
+        return input.substring(startIndex, endIndex);
+    }
+}
