c6186e706b479a73a08ffe43f90fd4e026444e4c
Felix Franz <felix-franz@t-online.de>
2017-10-21 09:43:19 +0200

    #29 added VLCConnector

---
+++ src/main/java/backend/general/connector/specialConnectors/VLCConnector.java
@@ -0,0 +1,60 @@
+package backend.general.connector.specialConnectors;
+
+import backend.general.connector.AbstractConnector;
+import backend.general.connector.universalConnector.Command;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Created by Felix on 21.10.2017.
+ */
+public class VLCConnector implements AbstractConnector {
+
+    private String name;
+    private String host;
+    private int port;
+    private String password;
+
+    private Socket connection = null;
+    private BufferedReader in = null;
+    private PrintWriter out = null;
+
+    public VLCConnector(String name, String host, int port, String password){
+        this.name = name;
+        this.host = host;
+        this.port = port;
+        this.password = password;
+    }
+
+    @Override
+    public boolean connect(){
+        try {
+            connection = new Socket(host, port);
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            out = new PrintWriter(new OutputStreamWriter(connection.getOutputStream()));
+            in.readLine();
+            out.println(password);
+            out.flush();
+            in.readLine();
+            if (in.readLine().equals("Welcome, Master")) return true;
+        } catch (IOException e) {
+            e.printStackTrace();    //ToDo handle exception
+        }
+        return false;
+    }
+
+    @Override
+    public boolean runCommand(Command command){
+        try {
+            out.println(command.getCommand());
+            out.flush();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+
+    }
+
+}
--- src/main/java/backend/general/connector/universalConnector/UniversalConnector.java
+++ src/main/java/backend/general/connector/universalConnector/UniversalConnector.java
@@ -1,9 +1,7 @@
 package backend.general.connector.universalConnector;
 
 import backend.general.connector.AbstractConnector;
-
-import java.io.*;
-import java.net.Socket;
+import backend.general.connector.specialConnectors.VLCConnector;
 
 /**
  * Created by Felix on 09.10.2017.
@@ -14,42 +12,32 @@ public class UniversalConnector implements AbstractConnector {
     private int port = 4212;
     private String password = "pass";
 
-    private Socket connection = null;
-    private BufferedReader in = null;
-    private PrintWriter out = null;
-
+    private transient AbstractConnector connector;
+    private transient boolean firstRun = true;
 
     protected UniversalConnector(){
     }
 
+    /**
+     * Creates a new instance of a connector at the first run
+     */
+    private void createInstanceOnFirstRun(){
+        if (firstRun){
+            connector = new VLCConnector(name, host, port, password);
+            firstRun = false;
+        }
+    }
+
     @Override
     public boolean connect(){
-        try {
-            connection = new Socket(host, port);
-            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            out = new PrintWriter(new OutputStreamWriter(connection.getOutputStream()));
-            in.readLine();
-            out.println(password);
-            out.flush();
-            in.readLine();
-            if (in.readLine().equals("Welcome, Master")) return true;
-        } catch (IOException e) {
-            e.printStackTrace();    //ToDo handle exception
-        }
-        return false;
+       createInstanceOnFirstRun();
+       return connector.connect();
     }
 
     @Override
     public boolean runCommand(Command command){
-        try {
-            out.println(command.getCommand());
-            out.flush();
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
-
+        createInstanceOnFirstRun();
+        return connector.runCommand(command);
     }
 
     /**
