23e3c7cffa44fa853bb367dfbf58a7859a7e9f48
Felix Franz <felix-franz@t-online.de>
2017-10-28 00:27:44 +0200

    #42 host links should be opend in a new tab

--- frontend.webapp/scriptServer.js
+++ frontend.webapp/scriptServer.js
@@ -17,7 +17,7 @@ function buildHost(host){
 	for (var i=0; i<host.length; i++){
 		content+=
 		'<div class = "col-sm-6 col-md-4">'
-			+ '<a href = "' + host[i].host + '" class = "thumbnail">'
+			+ '<a href = "' + host[i].host + '" class="thumbnail" target="_blank">'
 				+ '<img src = "' + host[i].qr + '" alt="' + host[i].host + '" title="Host address ' + host[i].host + '" />'
 				+ '<p class="caption" style="text-align: center;">' + host[i].host +'</p>'
 			+ '</a>'
