6a4b0f4b809e2e4d78c73b974d81b5b33d873a8e
Felix Franz <felix-franz@t-online.de>
2017-10-27 18:19:03 +0200

    #42 frontend shows qr code & host address

---
+++ frontend.webapp/scriptServer.js
@@ -0,0 +1,28 @@
+function loadHost(){
+	$.get(path + "server/host/",
+        /*{
+          client: "webapp"
+        }*/
+        null,
+        function(data,status){
+			if (status == "success")
+                buildHost(data)
+			else
+				alert("Failed to connect load host addresses!");
+        });
+}
+
+function buildHost(host){
+	var content = "";
+	console.log(host.length);
+	for (var i=0; i<host.length; i++){
+		content+=
+		'<div class = "col-sm-6 col-md-4">'
+			+ '<a href = "' + host[i].host + '" class = "thumbnail">'
+				+ '<img src = "' + host[i].qr + '" alt="' + host[i].host + '" title="Host address ' + host[i].host + '" />'
+				+ '<p class="caption" style="text-align: center;">' + host[i].host +'</p>'
+			+ '</a>'
+		+ '</div>';
+	}
+	$("#connectToServerConent").html(content);
+}
